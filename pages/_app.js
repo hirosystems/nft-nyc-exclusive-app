0e8f11fd534416a413e95393dc9d5d85b3e534a8
Alexander Graebe <agraebe@users.noreply.github.com>
2021-10-29 09:41:56 -0700

    fix: inter font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import 'inter-ui/inter.css';
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,10 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head />
-        <link
-          rel="stylesheet"
-          media="screen"
-          href="https://fontlibrary.org//face/open-sauce-one"
-          type="text/css"
-        />
         <link rel="icon" type="image/png" href="hiro-favicon.png"></link>
         <body>
           <Main />
